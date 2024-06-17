4b62b91b5acb54a3956a5afd63457bf19e7d1fb9
matheusgb <matheusgbr98@gmail.com>
2024-06-17 18:14:47 -0300

    chore: sets up path alias

---
+++ src/util/moduleAlias.ts
@@ -0,0 +1,9 @@
+import * as path from "path";
+import moduleAlias from "module-alias";
+
+const paths = path.resolve(__dirname, "../..");
+
+moduleAlias.addAliases({
+  "@src": path.join(paths, "src"),
+  "@test": path.join(paths, "test"),
+});
