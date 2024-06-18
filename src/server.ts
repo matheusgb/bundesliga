1e9d74687add3a27dc33ac1f11943452aef2d866
matheusgb <matheusgbr98@gmail.com>
2024-06-18 15:31:34 -0300

    feat: create SetupServer class

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import bodyParser from "body-parser";
+import "./util/moduleAlias";
+import { Server } from "@overnightjs/core";
+import { StandingsController } from "./controllers/standings";
+import { Application } from "express";
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+
+  private setupControllers(): void {
+    const standingsController = new StandingsController();
+    this.addControllers([standingsController]);
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
+}
