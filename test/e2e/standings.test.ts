cb27ae2d0674a8eda532723ab49e6328bb2b5ac5
matheusgb <matheusgbr98@gmail.com>
2024-06-18 15:35:09 -0300

    test: adjust first functional test

--- test/e2e/standings.test.ts
+++
@@ -1,9 +0,0 @@
-import supertest from "supertest";
-
-describe("Bundesliga 2023/24 standings e2e tests", () => {
-  it("should return Bundesliga 2023/24 season standings", async () => {
-    const { body, status } = await supertest(app).get("/standings");
-    expect(status).toBe(200);
-    expect(body).toBeTruthy();
-  });
-});
